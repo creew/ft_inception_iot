7dfff74f62f62e960daa6b29d446ad0256b2a5d6
eklompus <eklompus@student.21-school.ru>
2021-10-28 19:16:17 +0300

    added bash settings p1

---
+++ p1/scripts/init_bash.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo -i -u vagrant sh << EOF
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
+EOF
