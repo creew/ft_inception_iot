4b270f0516f98265b68e16e3d154e2f990a392d9
eklompus <eklompus@student.21-school.ru>
2021-10-31 19:07:23 +0300

    headers-based route for ingress

--- p2/scripts/access.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo -i -u vagrant bash << EOF
-export NODE_PORT=$(kubectl get services/app-one -o go-template='{{(index .spec.ports 0).targetPort}}')
-export NODE_PORT=$(kubectl get services/app-two -o go-template='{{(index .spec.ports 0).targetPort}}')
-export NODE_PORT=$(kubectl get services/app-three -o go-template='{{(index .spec.ports 0).targetPort}}')
-
-EOF
